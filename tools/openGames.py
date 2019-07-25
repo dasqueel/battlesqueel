155c9a97b32ce23a3f67ad7447dacce3f2128c4d
Neil Barduson <barduson.neil@gmail.com>
2019-07-25 13:01:10 -0500

    openGames update

--- tools/openGames.py
+++ tools/openGames.py
@@ -25,13 +25,14 @@ teamsMap = {
     "Charlotte": "char",
     "Cincinnati": "cin",
     "Clemson": "clem",
+    "Coastal Carolina": "ccu",
     "Colorado": "cu",
     "Colorado State": "csu",
     "Connecticut": "uconn",
     "Duke": "duke",
     "East Carolina": "ecu",
     "Eastern Michigan": "emu",
-    "FIU": "fiu",
+    "Florida International": "fiu",
     "Florida": "uf",
     "Florida Atlantic": "fau",
     "Florida State": "fsu",
@@ -51,9 +52,10 @@ teamsMap = {
     "Kansas State": "ksu",
     "Kent State": "kent",
     "Kentucky": "uk",
+    "Liberty": "lu",
     "LSU": "lsu",
     "Louisiana Tech": "ltu",
-    "Louisiana-Lafayette": "ull",
+    "Louisiana": "ull",
     "Louisiana-Monroe": "ulm",
     "Louisville": "ul",
     "Marshall": "marsh",
@@ -74,7 +76,7 @@ teamsMap = {
     "New Mexico": "unm",
     "New Mexico State": "nmst",
     "North Carolina": "unc",
-    "North Carolina State": "ncst",
+    "NC State": "ncst",
     "North Texas": "unt",
     "Northern Illinois": "niu",
     "Northwestern": "nu",
@@ -127,42 +129,67 @@ teamsMap = {
     "Western Michigan": "wmu",
     "Wisconsin": "wisc",
     "Wyoming": "wyo",
-    "byu": "byu",
-    "ole miss": "ole",
-    "smu": "smu",
-    "tcu": "tcu",
-    "ucf": "ucf",
-    "utep": "utep"
+    "BYU": "byu",
+    "Ole Miss": "ole",
+    "SMU": "smu",
+    "TCU": "tcu",
+    "UCF": "ucf",
+    "UTEP": "utep"
 }
 
 week = sys.argv[1]
+day = int(sys.argv[2]) # tu, w, th, f, sat OR 1 2 3 4
+# timeStart = sys.argv[3]
+# timeEnd = sys.argv[4]
 
 espnUrl = 'https://www.espn.com/college-football/schedule/_/week/' + week
 
-print espnUrl
-
 req = requests.get(espnUrl)
-# print req.text
 
 soup = BeautifulSoup(req.text, 'html.parser')
 
 # print soup
-#
+
 dayDivs = soup.findAll("table", {"class": "schedule has-team-logos align-left"})
 
 # get all trs in each dayDiv
-dayDiv = dayDivs[0]
+# dayDiv = dayDivs[0]
+dayDiv = dayDivs[day - 1]
 trs = dayDiv.findAll("tr")
 # first tr contains date information
 # rest of the trs are game rows
 
 for tr in trs:
+  # print tr
+  # teams are in spans
   spans = tr.findAll('span')
   # print spans
+
+  a = tr.find_all("a")
+
+  for x in a:
+    try:
+      print x.attrs['data-date']
+    except:
+      print 'nope'
+  # time = tr.findAll('a', {'data-dateformat': 'time1'})
+  # if len(time) > 0:
+    # z = time[0].text
+    # print z
   if len(spans) == 2:
-    for span in spans:
-      school = span.text
-      if school in teamsMap.keys(): print school, teamsMap[school]
+    awayTeam = ''
+    homeTeam = ''
+    # url = ''
+    for i, span in enumerate(spans):
+      school = span.text.replace('amp', '').replace(';','')
+      # print school
+      if school in teamsMap.keys():
+        # print school, teamsMap[school], i
+        if i == 0: awayTeam = teamsMap[school]
+        if i == 1: homeTeam = teamsMap[school]
+    if homeTeam != '' and awayTeam != '':
+      url = 'https://battlesqueel.herokuapp.com/' + awayTeam + '@' + homeTeam
+      # print url
 
 
 
